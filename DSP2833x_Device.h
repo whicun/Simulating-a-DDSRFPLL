397a49f8ebdb5e65b999e2b0ab148eef9275600e
Federico D. Ceccarelli <fededc88@gmail.com>
2021-11-08 17:29:01 -0300

    added DSP2833x devide header with data type declarations

---
+++ DSP2833x_Device.h
@@ -0,0 +1,31 @@
+//                                                                              
+// FILE:   DSP2833x_Device.h                                                    
+//                                                                              
+// TITLE:  My DSP2833x Device Definitions.                                         
+//                                                                              
+//###########################################################################   
+// $TI Release: DSP2833x/DSP2823x C/C++ Header Files V1.31 $                    
+// $Release Date: August 4, 2009 $                                              
+//###########################################################################   
+                                                                                
+#ifndef DSP2833x_DEVICE_H                                                       
+#define DSP2833x_DEVICE_H                                                      
+//---------------------------------------------------------------------------
+// For Portability, User Is Recommended To Use Following Data Type Size
+// Definitions For 16-bit and 32-Bit Signed/Unsigned Integers:
+//
+
+#ifndef DSP28_DATA_TYPES
+#define DSP28_DATA_TYPES
+typedef int                int16;
+typedef long               int32;
+typedef long long          int64;
+typedef unsigned char      Uint8;
+typedef unsigned int       Uint16;
+typedef unsigned long      Uint32;
+typedef unsigned long long Uint64;
+typedef float              float32;
+typedef long double        float64;
+#endif
+                                                                             
+#endif
