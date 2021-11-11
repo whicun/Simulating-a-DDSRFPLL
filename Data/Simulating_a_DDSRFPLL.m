25cfc19d2faa658100fd04ca4028d607679b2a63
Federico D. Ceccarelli <fededc88@gmail.com>
2021-11-11 16:44:46 -0300

    Completed matlab Data file with missing test, enhaced the dta.c and added a .h file to include in your project to have data available. Plot data was also added

--- Data/Simulating_a_DDSRFPLL.m
+++ Data/Simulating_a_DDSRFPLL.m
@@ -43,11 +43,14 @@ P.FimathDisplay='none';
 Fs=20000;
 Ts=1/Fs; 
 
-Tfinal=0.2; % Total time the simulation is run for
+Tfinal=2; % Total time the simulation is run for
 t=0:Ts:Tfinal; % time vector
 GridFreq=50; % nominal grid frequency
 wn=2*pi*GridFreq; % nominal frequency in radians
 fn=GridFreq;
+
+%disruption instant
+t_dis = Tfinal/0.2;
 
 %generate input signal
 wu=2*pi*GridFreq;
@@ -57,56 +60,70 @@ L=length(t);
 %% generate input signal and create a fi object of it
 %% CASE 1 : Phase Jump at the Mid Point
 
-for n=1:floor(L/2)
+for n=1:floor(L/t_dis)
 Ua(n)=cos(theta(n));
 Ub(n)=cos(theta(n)-2*pi/3);
 Uc(n)=cos(theta(n)-4*pi/3);
 end
-for n=floor(L/2):L
+for n=floor(L/t_dis):L
 Ua(n)=cos(theta(n)+1.5);
 Ub(n)=cos(theta(n)-2*pi/3+1.5);
 Uc(n)=cos(theta(n)-4*pi/3+1.5);
 end
+
 Ua_ideal=Ua;
 
 %% CASE 2: Unbalanced Grid
-% for n=1:floor(L)
-% Ua(n)=cos(theta(n));
-% Ub(n)=1.1*cos(theta(n)-2*pi/3);
-% Uc(n)=cos(theta(n)-4*pi/3);
-% end
-% Ua_ideal=cos(theta);
+
+for n=1:floor(L/t_dis)
+Ua(n)=cos(theta(n));
+Ub(n)=cos(theta(n)-2*pi/3);
+Uc(n)=cos(theta(n)-4*pi/3);
+end
+ for n=floor(L/t_dis):L
+ Ua(n)=cos(theta(n));
+ Ub(n)=1.1*cos(theta(n)-2*pi/3);
+ Uc(n)=cos(theta(n)-4*pi/3);
+ end
+ Ua_ideal=cos(theta);
 
 %% CASE 3: voltage harmonics
-% for n=1:floor(L)
-% Ua(n)=cos(theta(n))+0.05*cos(5*theta(n));
-% Ub(n)=cos(theta(n)-2*pi/3)+0.05*(cos(5*(theta(n)-2*pi/3)));
-% Uc(n)=cos(theta(n)-4*pi/3)+0.05*(cos(5*(theta(n)-4*pi/3)));
-% end
-% Ua_ideal=cos(theta);
+for n=1:floor(L/t_dis)
+Ua(n)=cos(theta(n));
+Ub(n)=cos(theta(n)-2*pi/3);
+Uc(n)=cos(theta(n)-4*pi/3);
+end
+
+ for n=floor(L/t_dis):L
+ Ua(n)=cos(theta(n))+0.05*cos(5*theta(n));
+ Ub(n)=cos(theta(n)-2*pi/3)+0.05*(cos(5*(theta(n)-2*pi/3)));
+ Uc(n)=cos(theta(n)-4*pi/3)+0.05*(cos(5*(theta(n)-4*pi/3)));
+ end
+ Ua_ideal=cos(theta);
 
 %% CASE 4: voltage dips and swells
-% %
-% for n=1:floor(L/2)
-% Ua(n)=cos(theta(n));
-% Ub(n)=cos(theta(n)-2*pi/3);
-% Uc(n)=cos(theta(n)-4*pi/3);
-% end
-% for n=floor(L/2):L
-% Ua(n)=0.7*cos(theta(n));
-% Ub(n)=0.7*cos(theta(n)-2*pi/3);
-% Uc(n)=0.7*cos(theta(n)-4*pi/3);
-% end
-% Ua_ideal=cos(theta);
+ %
+ for n=1:floor(L/2)
+ Ua(n)=cos(theta(n));
+ Ub(n)=cos(theta(n)-2*pi/3);
+ Uc(n)=cos(theta(n)-4*pi/3);
+ end
+ for n=floor(L/2):L
+ Ua(n)=0.7*cos(theta(n));
+ Ub(n)=0.7*cos(theta(n)-2*pi/3);
+ Uc(n)=0.7*cos(theta(n)-4*pi/3);
+ end
+ Ua_ideal=cos(theta);
 
 %% Plot and check data
-
+figure;
 plot(t,Ua,t,Ub,t,Uc);
 
 legend('Ua', 'Ub', 'Uc');
 
 %% Export Data to a .c file
 fid = fopen('data.c','wt');
+
 fprintf(fid,'float Ua[] = { \n');
 for i=1:length(Ua)
     fprintf(fid,'%.2f,\n',Ua(i));
@@ -130,3 +147,36 @@ end
     fprintf(fid,'};');
     
     fclose(fid);
+
+% Export Data to a .h file
+fid = fopen('data.h','wt');
+
+    fprintf(fid,'#ifndef __DATA_H__\n');
+    fprintf(fid,'#define __DATA_H__\n');
+    fprintf(fid,'\n');
+    fprintf(fid,'#define LEN_Un %d \n', length(Ua));
+    fprintf(fid,'\n');
+    fprintf(fid,'extern float Ua[];\n');
+    fprintf(fid,'extern float Ub[];\n');
+    fprintf(fid,'extern float Uc[];\n');
+    fprintf(fid,'#endif');
+
+fclose(fid);
+
+%% plot results
+% Import data
+filename = 'Testddsrfpll.txt';
+delimiterIn = ' ';
+headerlinesIn = 1;
+A = importdata(filename,delimiterIn,headerlinesIn);
+
+figure;
+ax1 = subplot(3,1,1), plot(t,Ua,t,Ub,t,Uc), legend('Ua', 'Ub', 'Uc'), title('Ua, Ub, Uc');
+ax2 = subplot(3,1,2), plot(t,A.data(:,3),t,A.data(:,4)),legend('Alpha', 'Beta'), title('Alpha Beta');
+ax3 = subplot(3,1,3), plot(t,A.data(:,1),t,A.data(:,2)),legend('Uq', 'Ud'), title('Ud, Uq, decoupled');
+linkaxes([ax1,ax2, ax3],'x');
+
+figure;
+ax1 = subplot(2,1,1), plot(t,Ua_ideal,t,A.data(:,5)), legend('Ua Ideal', 'PLL cos(theta)'), title('Ideal vs Locked AC');
+ax2 = subplot(2,1,2), plot(t,Ua_ideal' - A.data(:,5)), title('Error');
+linkaxes([ax1,ax2],'x');
